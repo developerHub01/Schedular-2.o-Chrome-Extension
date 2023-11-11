a9eadbdd60f036a1934ef711205ee1e397ce6bf6
developerHub01 <developerhubytchannel@gmail.com>
2023-11-12 05:00:18 +0600

    regularType notification added

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.scheduleType === "oneTimeSchedule") {
-    console.log(message.scheduleType);
-    handleAlarmsForOneTime();
-  } else if (message.scheduleType === "regularTimeSchedule") {
-    console.log(message.scheduleType);
-  } else {
+  if (message.scheduleType === "oneTimeSchedule") handleAlarmsForOneTime();
+  else if (message.scheduleType === "regularTimeSchedule")
+    handleAlarmsForRegularTime();
+  else {
     console.log(message.scheduleType);
   }
 });
@@ -12,7 +10,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 const handleAlarmsForOneTime = () => {
   chrome.storage.local.get("oneTimeSchedule").then((result) => {
     const scheduleList = result.oneTimeSchedule;
-    console.log("===================");
+
     if (!scheduleList) return;
     Object.keys(scheduleList).forEach((item) => {
       // To set the schedule first clearing all if it aleary sets then it will update
@@ -28,8 +26,6 @@ const handleAlarmsForOneTime = () => {
 
       const delay = scheduledTime - currentTime;
 
-      console.log(scheduleList[item]);
-
       // if schedule day is earlier than current date
       if (delay <= 0) {
         delete scheduleList[item];
@@ -37,10 +33,36 @@ const handleAlarmsForOneTime = () => {
           oneTimeSchedule: scheduleList,
         });
       } else {
-        chrome.alarms.create(item + "-" + "oneTimeSchedule", {
+        chrome.alarms.create(`${item}-oneTimeSchedule`, {
           when: currentTime + delay,
         });
       }
+    });
+  });
+};
+const handleAlarmsForRegularTime = () => {
+  chrome.storage.local.get("regularTimeSchedule").then((result) => {
+    const scheduleList = result.regularTimeSchedule;
+
+    if (!scheduleList) return;
+    Object.keys(scheduleList).forEach((item) => {
+      console.log(scheduleList[item]);
+      const { taskTime } = scheduleList[item];
+      const [hours, minutes] = taskTime.split(":");
+      console.log(hours, minutes);
+      const scheduledTime = new Date();
+      scheduledTime.setHours(hours, minutes, 0, 0);
+      const currentTime = new Date();
+
+      if (scheduledTime < currentTime) {
+        scheduledTime.setDate(scheduledTime.getDate() + 1);
+      }
+      const timeGap = scheduledTime - currentTime;
+      console.log(`${item}-regularTimeSchedule`);
+      chrome.alarms.create(`${item}-regularTimeSchedule`, {
+        when: Date.now() + timeGap,
+        periodInMinutes: 24 * 60,
+      });
     });
   });
 };
@@ -49,7 +71,9 @@ chrome.alarms.onAlarm.addListener((alarm) => {
   console.log(id, scheduleType);
   chrome.storage.local.get(scheduleType).then((result) => {
     const schedules = result[scheduleType];
+    console.log(schedules);
     const scheduleData = schedules[id];
+    console.log(scheduleData);
     const notificationOptions = {
       type: "basic",
       title: scheduleData.taskTitle,
@@ -58,11 +82,14 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     };
     delete schedules[id];
     console.log(schedules);
-    chrome.storage.local.set({ [scheduleType]: schedules });
-    chrome.notifications.create("", notificationOptions);
 
-    chrome.runtime.sendMessage({
-      type: scheduleType,
-    });
+    chrome.notifications.create("", notificationOptions);
+    if (scheduleType === "oneTimeSchedule") {
+      chrome.storage.local.set({ [scheduleType]: schedules });
+
+      chrome.runtime.sendMessage({
+        type: scheduleType,
+      });
+    }
   });
 });
